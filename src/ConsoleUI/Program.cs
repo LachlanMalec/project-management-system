bb4e51fcb43fa89ebdca54f7b7cc8fae6d90e703
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-05-27 15:53:18 +1000

    feat(ui): display logic

--- src/ConsoleUI/Program.cs
+++ src/ConsoleUI/Program.cs
@@ -1,3 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ProjectManagementSystem.App;
+using ProjectManagementSystem.App.Commands;
+namespace ProjectManagementSystem.ConsoleUI;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var state = new State();
+        string? currentFilePath = null;
+        var view = new Interface();
+        view.ShowSplash();
+
+        while (true)
+        {
+            var mainMenuChoice = view.ShowMainMenu();
+            switch (mainMenuChoice)
+            {
+                case "Create Task":
+                    var newTaskId = view.ShowCreateTaskIdPrompt();
+                    var newTaskTimeToComplete = int.Parse(view.ShowCreateTaskTimeToCompletePrompt());
+                    var newTaskDependencies = view.ShowCreateTaskDependenciesPrompt().Split(",").ToList();
+                    newTaskDependencies.RemoveAll(s => s == "");
+                    new CreateTaskCommand(newTaskId, newTaskTimeToComplete, newTaskDependencies).Execute(state);
+                    break;
+                case "Import Tasks (from file)":
+                    switch (view.ShowImportTasksConfirmation())
+                    {
+                        case "Continue":
+                            var filePath = view.ShowImportTasksFilePrompt();
+                            new ImportTasksCommand(filePath).Execute(state);
+                            currentFilePath = filePath;
+                            break;
+                        case "Back":
+                            break;
+                    }
+                    break;
+                case "Save Tasks (to file)":
+                    switch(view.ShowSaveTasksConfirmation())
+                    {
+                        case "Continue":
+                            if (currentFilePath == null)
+                            {
+                                var filePath = view.ShowSaveTasksFilePrompt();
+                                new SaveTasksCommand(filePath).Execute(state);
+                                currentFilePath = filePath;
+                            }
+                            else
+                            {
+                                new SaveTasksCommand(currentFilePath).Execute(state);
+                            }
+                            break;
+                        case "Back":
+                            break;
+                    }
+                    break;
+                case "Exit":
+                    switch (view.ShowExitConfirmation())
+                    {
+                        case "Exit":
+                            Environment.Exit(0);
+                            break;
+                        case "Back":
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+}
