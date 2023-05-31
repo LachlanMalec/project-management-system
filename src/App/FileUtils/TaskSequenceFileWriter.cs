637806bff74facffbb924c61ebba8ec2125eb853
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-05-31 14:42:08 +1000

    fix(app): sequence file writer was inserting incorrect spaces

--- src/App/FileUtils/TaskSequenceFileWriter.cs
+++ src/App/FileUtils/TaskSequenceFileWriter.cs
@@ -13,10 +13,10 @@ public static class TaskSequenceFileWriter
     {
         // Get the IDs of the tasks in the order they appear in the collection
         var taskIds = tasks.Select(task => task.Id).ToList();
-        // Add a space to the end of every task ID as per the spec
-        for(var i = 0; i < taskIds.Count; i++)
+        // Add a space to the beginning of each ID, except the first one
+        for (var i = 1; i < taskIds.Count; i++)
         {
-            taskIds[i] += " ";
+            taskIds[i] = $" {taskIds[i]}";
         }
         if (File.Exists(filePath)) File.Delete(filePath);
         await using var writer = new StreamWriter(filePath);
