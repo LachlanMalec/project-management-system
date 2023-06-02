f873aab69b7787616ed7e8fb71c69fb6a48dcd28
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-02 12:10:46 +1000

    chore(app): fully document save optimized tasks command

--- src/App/Commands/SaveOptimizedTasksCommand.cs
+++ src/App/Commands/SaveOptimizedTasksCommand.cs
@@ -1,8 +1,15 @@
 using ProjectManagementSystem.App.FileUtils;
 namespace ProjectManagementSystem.App.Commands;
 
+/// <summary>
+/// A command to save the optimized tasks to a file.
+/// </summary>
 public class SaveOptimizedTasksCommand
 {
+    /// <summary>
+    /// Executes the command.
+    /// </summary>
+    /// <param name="state">The state to execute the command on.</param>
     public async Task Execute(State state)
     {
         await TaskStartTimesFileWriter.Write("EarliestTimes.txt", await state.OptimizedTasks());
