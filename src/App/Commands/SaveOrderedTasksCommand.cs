48486c0666f814b3c0cedc9ecde7516667a39bb1
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-02 12:10:30 +1000

    chore(app): fully document save ordered tasks command

--- src/App/Commands/SaveOrderedTasksCommand.cs
+++ src/App/Commands/SaveOrderedTasksCommand.cs
@@ -1,8 +1,15 @@
 using ProjectManagementSystem.App.FileUtils;
 namespace ProjectManagementSystem.App.Commands;
 
+/// <summary>
+/// A command to import tasks from a file.
+/// </summary>
 public class SaveOrderedTasksCommand
 {
+    /// <summary>
+    /// Executes the command.
+    /// </summary>
+    /// <param name="state">The state to execute the command on.</param>
     public async Task Execute(State state)
     {
         await TaskSequenceFileWriter.Write("Sequence.txt", await state.OrderedTasks());
