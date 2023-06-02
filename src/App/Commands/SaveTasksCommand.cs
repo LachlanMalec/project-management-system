3dd83098c69324378449bfeb0b5234b7bcd62bc1
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-02 12:10:15 +1000

    chore(app): fully document save task command

--- src/App/Commands/SaveTasksCommand.cs
+++ src/App/Commands/SaveTasksCommand.cs
@@ -1,14 +1,26 @@
 using ProjectManagementSystem.App.FileUtils;
 namespace ProjectManagementSystem.App.Commands;
 
+/// <summary>
+/// A command to save the tasks to a file.
+/// </summary>
 public class SaveTasksCommand
 {
     private readonly string _filePath;
+    
+    /// <summary>
+    /// Creates a command that when executed will save the tasks to a file.
+    /// </summary>
+    /// <param name="filePath">The path to the file to save the tasks to.</param>
     public SaveTasksCommand(string filePath)
     {
         _filePath = filePath;
     }
     
+    /// <summary>
+    /// Executes the command.
+    /// </summary>
+    /// <param name="state">The state to execute the command on.</param>
     public async Task Execute(State state)
     {
         await TaskFileWriter.Write(_filePath, state.Tasks);
